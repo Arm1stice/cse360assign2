5ec2e474835a5c745d70ff90c300e74b68bd1788
Arm1stice <arm1stice@arm1stice.com>
2019-10-05 09:29:26 -0700

    Initial commit with JavaDoc

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,61 @@
+/** CSE360 Assignment 2
+ *  Wyatt Calandro
+ */
+package cse360assign2;
+
+/**
+ * AddingMachine is a class for adding and subtracting
+ */
+public class AddingMachine {
+
+    /**
+     * The current sum
+     */
+	private int total;
+	
+	/**
+	 * Instantiate new class, initializing total to 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Get the current total
+	 * @return Current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add to current total
+	 * @param value the value to add to the current total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+     * Subtract from current total
+     * @param value the value to subtract from the current total.
+     */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Convert calculation to string.
+	 * @return string representing all math operations done to get total.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Reset total to 0.
+	 */
+	public void clear() {
+	
+	}
+}
